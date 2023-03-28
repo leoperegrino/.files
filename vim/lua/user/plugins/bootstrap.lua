6640845da2df8962a8d0e33fe79b64d76156d7f2
ltp <leo@ltp.pt>
2023-03-28 18:59:27 -0300

    change in XDG standard

--- vim/lua/user/plugins/bootstrap.lua
+++ vim/lua/user/plugins/bootstrap.lua
@@ -1,4 +1,4 @@
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath('state')..'/site/pack/packer/start/packer.nvim'
 local packer_url =  'https://github.com/wbthomason/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
