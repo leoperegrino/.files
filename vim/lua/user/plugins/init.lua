1c8801ee23b2e733ec6be38e2794f2c020262249
ltp <leo@ltp.pt>
2023-04-03 20:29:48 -0300

    separated packer code from plugins `init.lua`

---
+++ vim/lua/user/plugins/init.lua
@@ -0,0 +1,2 @@
+require('user.plugins.bootstrap')
+require('user.plugins.packer').setup()
--- vim/lua/user/plugins/packer.lua
+++ vim/lua/user/plugins/packer.lua
@@ -1,80 +1,83 @@
-require("user.plugins.bootstrap")
+local M = {}
 
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
+local packer = require("packer")
+local packer_util = require("packer.util")
+
+M.setup = function()
+
+	packer.reset()
+	packer.init {
+		display = {
+			open_fn = function()
+				return packer_util.float { border = "rounded" }
+			end,
+		},
+	}
+
+	packer.startup(
+		function(use)
+			use 'wbthomason/packer.nvim'
+			use 'nvim-lua/plenary.nvim'
+
+			use 'neovim/nvim-lspconfig'
+			use "williamboman/mason.nvim"
+			use "williamboman/mason-lspconfig.nvim"
+			use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+			use 'nvim-treesitter/nvim-treesitter-context'
+			use 'jose-elias-alvarez/null-ls.nvim'
+
+			use 'mfussenegger/nvim-dap'
+			use 'rcarriga/nvim-dap-ui'
+			use 'theHamsta/nvim-dap-virtual-text'
+			use 'mfussenegger/nvim-dap-python'
+			-- use 'nvim-telescope/telescope-dap.nvim'
+
+			use 'hrsh7th/nvim-cmp'
+			use 'hrsh7th/cmp-buffer'
+			use 'hrsh7th/cmp-path'
+			use 'hrsh7th/cmp-cmdline'
+			use 'hrsh7th/cmp-nvim-lsp-signature-help'
+			use 'hrsh7th/cmp-nvim-lsp'
+			use 'hrsh7th/cmp-nvim-lua'
+			use 'L3MON4D3/LuaSnip'
+			use 'saadparwaiz1/cmp_luasnip'
+			use 'rafamadriz/friendly-snippets'
+
+			use { 'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" } }
+			use 'simrat39/rust-tools.nvim'
+
+			-- use 'chentoast/marks.nvim'
+			use 'numToStr/Comment.nvim'
+			use { 'windwp/nvim-autopairs', config = function() require("nvim-autopairs").setup {} end }
+			use 'akinsho/toggleterm.nvim'
+			use 'kyazdani42/nvim-tree.lua'
+			-- use 'ray-x/lsp_signature.nvim'
+			use 'nvim-telescope/telescope.nvim'
+			use 'debugloop/telescope-undo.nvim'
+
+			use 'ajmwagar/vim-deus'
+			use 'p00f/nvim-ts-rainbow'
+			use 'stevearc/dressing.nvim'
+			use 'kyazdani42/nvim-web-devicons'
+			use 'simrat39/symbols-outline.nvim'
+			use 'nvim-lualine/lualine.nvim'
+			-- use 'arkav/lualine-lsp-progress'
+			use 'j-hui/fidget.nvim'
+			use "lukas-reineke/indent-blankline.nvim"
+			use 'lewis6991/gitsigns.nvim'
+
+			if PACKER_BOOTSTRAP then
+				require('packer').sync()
+			end
+		end
+	)
+
+	vim.api.nvim_create_augroup("packer_user_config", {})
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = "packer_user_config",
+		pattern = "*/user/plugins/packer.lua",
+		command = [[source <afile> | PackerSync]]
+	})
 end
 
-packer.reset()
-packer.init {
-	display = {
-		open_fn = function()
-			return require("packer.util").float { border = "rounded" }
-		end,
-	},
-}
-
-packer.startup(
-	function(use)
-		use 'wbthomason/packer.nvim'
-		use 'nvim-lua/plenary.nvim'
-
-		use 'neovim/nvim-lspconfig'
-		use "williamboman/mason.nvim"
-		use "williamboman/mason-lspconfig.nvim"
-		use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-		use 'nvim-treesitter/nvim-treesitter-context'
-		use 'jose-elias-alvarez/null-ls.nvim'
-
-		use 'mfussenegger/nvim-dap'
-		use 'rcarriga/nvim-dap-ui'
-		use 'theHamsta/nvim-dap-virtual-text'
-		use 'mfussenegger/nvim-dap-python'
-		-- use 'nvim-telescope/telescope-dap.nvim'
-
-		use 'hrsh7th/nvim-cmp'
-		use 'hrsh7th/cmp-buffer'
-		use 'hrsh7th/cmp-path'
-		use 'hrsh7th/cmp-cmdline'
-		use 'hrsh7th/cmp-nvim-lsp-signature-help'
-		use 'hrsh7th/cmp-nvim-lsp'
-		use 'hrsh7th/cmp-nvim-lua'
-		use 'L3MON4D3/LuaSnip'
-		use 'saadparwaiz1/cmp_luasnip'
-		use 'rafamadriz/friendly-snippets'
-
-		use { 'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" } }
-		use 'simrat39/rust-tools.nvim'
-
-		-- use 'chentoast/marks.nvim'
-		use 'numToStr/Comment.nvim'
-		use { 'windwp/nvim-autopairs', config = function() require("nvim-autopairs").setup {} end }
-		use 'akinsho/toggleterm.nvim'
-		use 'kyazdani42/nvim-tree.lua'
-		-- use 'ray-x/lsp_signature.nvim'
-		use 'nvim-telescope/telescope.nvim'
-		use 'debugloop/telescope-undo.nvim'
-
-		use 'ajmwagar/vim-deus'
-		use 'p00f/nvim-ts-rainbow'
-		use 'stevearc/dressing.nvim'
-		use 'kyazdani42/nvim-web-devicons'
-		use 'simrat39/symbols-outline.nvim'
-		use 'nvim-lualine/lualine.nvim'
-		-- use 'arkav/lualine-lsp-progress'
-		use 'j-hui/fidget.nvim'
-		use "lukas-reineke/indent-blankline.nvim"
-		use 'lewis6991/gitsigns.nvim'
-
-		if PACKER_BOOTSTRAP then
-			require('packer').sync()
-		end
-	end
-)
-
-vim.api.nvim_create_augroup("packer_user_config", {})
-vim.api.nvim_create_autocmd("BufWritePost", {
-	group = "packer_user_config",
-	pattern = "*/user/plugins/init.lua",
-	command = [[source <afile> | PackerSync]]
-})
+return M
