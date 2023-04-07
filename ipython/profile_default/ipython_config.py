e13e0cd72f6585555f07d909422dfdbac7909b0e
ltp <leo@ltp.pt>
2023-04-07 16:29:30 -0300

    moved history to xdg's state directory

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,5 +1,9 @@
 import os
-cache = os.environ.get('XDG_CACHE_HOME', os.path.expanduser('~/.cache'))
+
+state = os.environ.get(
+    'XDG_STATE_HOME',
+    os.path.expanduser('~/.local/state')
+)
 
 c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.confirm_exit = False
@@ -8,7 +12,7 @@ c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 c.TerminalInteractiveShell.space_for_menu = 20
-c.TerminalInteractiveShell.debugger_history_file = f'{cache}/ipython/pdbhistory'
-c.HistoryAccessor.hist_file = f'{cache}/ipython/history.sqlite'
+c.TerminalInteractiveShell.debugger_history_file = f'{state}/ipython/pdbhistory'
+c.HistoryAccessor.hist_file = f'{state}/ipython/history.sqlite'
 c.Application.verbose_crash=True
 c.InteractiveShell.autocall = 1
