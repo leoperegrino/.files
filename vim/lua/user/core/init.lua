bd6edcaba9ff7d42a9a72dd2cdd7b64b88e349f8
leoperegrino <55335068+leoperegrino@users.noreply.github.com>
2023-04-21 13:52:49 -0300

    removed unnecessary export of `lsp` and `keymaps`

--- vim/lua/user/core/init.lua
+++ vim/lua/user/core/init.lua
@@ -1,18 +1,18 @@
 local M = {}
 
-M.keymaps = require('user.core.keymaps')
-M.lsp = require('user.core.lsp')
+local keymaps = require('user.core.keymaps')
+local lsp = require('user.core.lsp')
 
 
 M.on_attach = function(client, bufnr)
-	M.lsp.on_attach(client, bufnr)
-	M.keymaps.on_attach(client, bufnr)
+	lsp.on_attach(client, bufnr)
+	keymaps.on_attach(client, bufnr)
 end
 
 
 M.setup = function()
-	M.lsp.setup()
-	M.keymaps.setup()
+	lsp.setup()
+	keymaps.setup()
 end
 
 
