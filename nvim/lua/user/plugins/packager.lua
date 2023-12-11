710d8909d464f8276f78b7a00a0b149bb7346d6f
ltp <leo@ltp.pt>
2023-12-11 12:10:58 -0300

    fix: update telescope to fix exceptions

--- nvim/lua/user/plugins/packager.lua
+++ nvim/lua/user/plugins/packager.lua
@@ -71,7 +71,7 @@ M.setup = function()
 			use {
 				{ 'nvim-telescope/telescope.nvim',
 					config = c.telescope,
-					tag = '0.1.1',
+					tag = '0.1.4',
 					requires = { 'nvim-lua/plenary.nvim' }
 				},
 				{ 'debugloop/telescope-undo.nvim'         },
