aaac73f43aca3851325f0170b1f12823c174669f
Leonardo Teixeira Peregrino <leo@ltp.pt>
2024-01-12 18:07:31 -0300

    change to lazy package manager

--- nvim/lua/user/plugins/packager.lua
+++ nvim/lua/user/plugins/packager.lua
@@ -2,26 +2,27 @@ local M = {}
 
 local c = require('user.plugins.config')
 
-local pack = vim.fn.stdpath('data') .. '/site/pack'
-
 
 local bootstrap = function()
-	local install_path = pack .. '/packer' .. '/start' .. '/packer.nvim'
-	local packer_url =  'https://github.com/wbthomason/packer.nvim'
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	local lazy_url =  'https://github.com/folke/lazy.nvim.git'
 	local bootstrapped = false
 
-	if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-		bootstrapped = vim.fn.system({
-			'git',
-			'clone',
-			'--depth', '1',
-			packer_url,
-			install_path
+
+	if not vim.loop.fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			lazy_url,
+			"--branch=stable", -- latest stable release
+			lazypath,
 		})
 
-		vim.notify('Installing packer close and reopen Neovim...')
-		vim.cmd([[packadd packer.nvim]])
 	end
+
+	vim.opt.rtp:prepend(lazypath)
+	bootstrapped = true
 
 	return bootstrapped
 end
@@ -29,86 +30,62 @@ end
 
 M.setup = function()
 	local bootstrapped = bootstrap()
-	local packer = require('packer')
-	local util = require('packer.util')
+	local lazy = require('lazy')
 
-	packer.startup({
-		function(use)
-			use {
-				{ 'wbthomason/packer.nvim' },
-				{ 'nvim-lua/plenary.nvim'  },
-			}
+	lazy.setup({
+		{ 'nvim-lua/plenary.nvim'  },
 
-			use {
-				{ 'neovim/nvim-lspconfig' },
-				{ 'williamboman/mason.nvim', run = ':MasonUpdate' },
-				{ 'williamboman/mason-lspconfig.nvim' },
-				{ 'jose-elias-alvarez/null-ls.nvim', requires = 'nvim-lua/plenary.nvim' },
-			}
+		{ 'neovim/nvim-lspconfig' },
+		{ 'williamboman/mason.nvim', build = ':MasonUpdate' },
+		{ 'williamboman/mason-lspconfig.nvim' },
+		{ 'jose-elias-alvarez/null-ls.nvim', dependencies = 'nvim-lua/plenary.nvim' },
 
-			use { 'scalameta/nvim-metals', requires = { 'nvim-lua/plenary.nvim' } }
-			use { 'simrat39/rust-tools.nvim' }
+		{ 'scalameta/nvim-metals', dependencies = { 'nvim-lua/plenary.nvim' } },
+		{ 'simrat39/rust-tools.nvim' },
 
-			use {
-				{ 'nvim-treesitter/nvim-treesitter'        , config = c.treesitter, run = ':TSUpdate' },
-				{ 'nvim-treesitter/nvim-treesitter-context', config = c.context },
-				{ 'nvim-treesitter/playground' },
-				{ 'HiPhish/nvim-ts-rainbow2' },
-			}
+		{ 'nvim-treesitter/nvim-treesitter'        , config = c.treesitter, build = ':TSUpdate' },
+		{ 'nvim-treesitter/nvim-treesitter-context', config = c.context },
+		{ 'nvim-treesitter/playground' },
+		{ 'HiPhish/nvim-ts-rainbow2' },
 
-			use {
-				{ 'hrsh7th/nvim-cmp',    config = c.cmp },
-				{ 'hrsh7th/cmp-buffer'                  },
-				{ 'hrsh7th/cmp-path'                    },
-				{ 'hrsh7th/cmp-cmdline'                 },
-				{ 'hrsh7th/cmp-nvim-lsp-signature-help' },
-				{ 'hrsh7th/cmp-nvim-lsp'                },
-				{ 'hrsh7th/cmp-nvim-lua'                },
-				{ 'saadparwaiz1/cmp_luasnip'            },
-				{ 'L3MON4D3/LuaSnip', requires = { 'rafamadriz/friendly-snippets'} },
-			}
+		{ 'hrsh7th/nvim-cmp',    config = c.cmp },
+		{ 'hrsh7th/cmp-buffer'                  },
+		{ 'hrsh7th/cmp-path'                    },
+		{ 'hrsh7th/cmp-cmdline'                 },
+		{ 'hrsh7th/cmp-nvim-lsp-signature-help' },
+		{ 'hrsh7th/cmp-nvim-lsp'                },
+		{ 'hrsh7th/cmp-nvim-lua'                },
+		{ 'saadparwaiz1/cmp_luasnip'            },
+		{ 'L3MON4D3/LuaSnip', dependencies = { 'rafamadriz/friendly-snippets'} },
 
-			use {
-				{ 'nvim-telescope/telescope.nvim',
-					config = c.telescope,
-					tag = '0.1.4',
-					requires = { 'nvim-lua/plenary.nvim' }
-				},
-				{ 'debugloop/telescope-undo.nvim'         },
-				{ 'nvim-telescope/telescope-symbols.nvim' },
-			}
+		{ 'nvim-telescope/telescope.nvim',
+			config = c.telescope,
+			tag = '0.1.4',
+			dependencies = { 'nvim-lua/plenary.nvim' }
+		},
+		{ 'debugloop/telescope-undo.nvim'         },
+		{ 'nvim-telescope/telescope-symbols.nvim' },
 
-			use {
-				{ 'kyazdani42/nvim-tree.lua'    , config = c.nvim_tree },
-				{ 'nvim-lualine/lualine.nvim'   , config = c.lualine   },
-				{ 'nvim-tree/nvim-web-devicons' }
-			}
+		{ 'kyazdani42/nvim-tree.lua'    , config = c.nvim_tree },
+		{ 'nvim-lualine/lualine.nvim'   , config = c.lualine   },
+		{ 'nvim-tree/nvim-web-devicons' },
 
-			use { 'numToStr/Comment.nvim'              , config = c.comment   }
-			use { 'windwp/nvim-autopairs'              , config = c.autopairs }
-			use { 'lewis6991/gitsigns.nvim'            , config = c.gitsigns  }
+		{ 'numToStr/Comment.nvim'              , config = c.comment   },
+		{ 'windwp/nvim-autopairs'              , config = c.autopairs },
+		{ 'lewis6991/gitsigns.nvim'            , config = c.gitsigns  },
 
-			use { "lukas-reineke/indent-blankline.nvim", main = 'ibl', config = c.blankline }
-			use { 'stevearc/dressing.nvim'             , config = c.dressing  }
-			use { 'Mofiqul/vscode.nvim'                , config = c.vscode    }
-			use { "catppuccin/nvim", as = "catppuccin" , config = c.catppuccin }
-
-			if bootstrapped then
-				packer.sync()
-			end
-		end,
-		config = {
-			-- https://github.com/wbthomason/packer.nvim/issues/201#issuecomment-1011066526
-			compile_path = util.join_paths(
-				pack, 'loader', 'start', 'packer.nvim', 'plugin', 'packer.lua'
-			),
-			display = {
-				open_fn = function()
-					return util.float({ border = 'rounded' })
-				end,
-			}
+		{ "lukas-reineke/indent-blankline.nvim", main = 'ibl', config = c.blankline },
+		{ 'stevearc/dressing.nvim'             , config = c.dressing  },
+		{ 'Mofiqul/vscode.nvim'                , config = c.vscode    },
+		{ "catppuccin/nvim", name = "catppuccin" , config = c.catppuccin },
+	},
+	{
+		lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json",
+		ui = {
+			border = "rounded"
 		}
 	})
+
 end
 
 
