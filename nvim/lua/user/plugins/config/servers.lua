f95c2eb2cdc42ac6d830f58fa78b76526ae78d66
Leonardo Teixeira Peregrino <leo@ltp.pt>
2024-01-12 18:17:56 -0300

    improve documentation of python lsp features

--- nvim/lua/user/plugins/config/servers.lua
+++ nvim/lua/user/plugins/config/servers.lua
@@ -27,12 +27,42 @@ M.texlab = {
 	}
 }
 
+-- pip install "python-lsp-server[all]
 M.pylsp = {
+	-- cmd = { 'python',  '-m', 'pylsp' },
 	settings = {
 		pylsp = {
 			plugins = {
+				jedi_hover = { enabled = true },
+
+				-- optional providers
+
+				-- for code formatting, prefer yapf
+				autopep8 = { enabled = false },
+
+				-- for error checking (disabled by default)
+				flake8 = { enabled = false },
+
+				-- linter for complexity checking
+				mccabe = { enabled = true },
+
+				-- linter for style checking
+				pycodestyle = { enabled = true },
+
+				-- linter for docstring style checking (disabled by default)
+				pydocstyle = { enabled = false, convention = 'pep257', addIgnore = { 'D100' } },
+
+				-- linter to detect various errors
 				pyflakes = { enabled = false },
-				jedi_hover = { enabled = false },
+
+				-- for code linting (disabled by default)
+				pylint = { enabled = true, args = { '--disable=C0114,C0115,C0116'} },
+
+				-- for Completions and renaming
+				rope = { enabled = true },
+
+				-- for code formatting (preferred over autopep8)
+				yapf = { enabled = true },
 			}
 		}
 	}
