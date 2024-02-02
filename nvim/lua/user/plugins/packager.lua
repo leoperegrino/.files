6ec44d2da39101c7fdc8e986326dc48c60b40cc4
leoperegrino <55335068+leoperegrino@users.noreply.github.com>
2024-02-02 16:44:02 -0300

    Update packager.lua
    fix Lazy lazy loading vscode theme and not applying to lualine

--- nvim/lua/user/plugins/packager.lua
+++ nvim/lua/user/plugins/packager.lua
@@ -67,7 +67,7 @@ M.setup = function()
 		{ 'nvim-telescope/telescope-symbols.nvim' },
 
 		{ 'kyazdani42/nvim-tree.lua'    , config = c.nvim_tree },
-		{ 'nvim-lualine/lualine.nvim'   , config = c.lualine   },
+		{ 'nvim-lualine/lualine.nvim'   , config = c.lualine   , dependencies = 'Mofiqul/vscode.nvim' },
 		{ 'nvim-tree/nvim-web-devicons' },
 
 		{ 'numToStr/Comment.nvim'              , config = c.comment   },
