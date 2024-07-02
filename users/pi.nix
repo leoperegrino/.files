2b4bb62e7fc256b515b65cf04455b2f2f404e40e
ltp <leo@ltp.pt>
2024-07-02 19:53:19 -0300

    use same config structure for pi

--- users/pi.nix
+++ users/pi.nix
@@ -5,6 +5,22 @@
 	...
 }: {
 	imports = [
+		../modules
 	];
 
+	modules.enable = true;
+
+	home.username = "pi";
+	home.homeDirectory = "/home/pi";
+
+	programs.home-manager.enable = true;
+
+
+	home.packages = with pkgs; [
+	];
+
+	systemd.user.startServices = "sd-switch";
+
+	# https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+	home.stateVersion = "23.11";
 }
