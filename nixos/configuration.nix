6bb71d3e26be4634d56b6169b743342e2cdb4705
ltp <leo@ltp.pt>
2024-07-02 16:49:51 -0300

    auto gc

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,6 +8,7 @@
 	nix.settings.use-xdg-base-directories = true;
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 	nix.gc.options = "--delete-older-than 7d";
+	nix.gc.automatic = true;
 
 	boot = {
 		consoleLogLevel = 0;
