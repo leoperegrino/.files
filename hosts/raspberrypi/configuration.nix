1d02cc423539dcbeb5ea01035fd5514e559cee00
ltp <leo@ltp.pt>
2024-07-14 13:18:03 -0300

    fix: profile.local is not being read for some reason

--- hosts/raspberrypi/configuration.nix
+++ hosts/raspberrypi/configuration.nix
@@ -122,6 +122,7 @@
 		export        XDG_STATE_HOME="''${HOME}/.local/state"
 		export        XDG_CACHE_HOME="''${HOME}/.cache"
 		export       XDG_CONFIG_HOME="''${HOME}/.config"
+		source "''${XDG_CONFIG_HOME}/sh/profile"
 	'';
 
 	environment.etc."zshenv.local".text = ''
@@ -129,9 +130,6 @@
 	'';
 
 	environment.etc."profile.local".text = let profile = "\${XDG_CONFIG_HOME}/sh/profile"; in ''
-		if test -f "${profile}"; then
-			source "${profile}"
-		fi
 	'';
 
 	environment.etc."bashrc.local".text = let bashrc = "\${XDG_CONFIG_HOME}/bash/bashrc"; in ''
