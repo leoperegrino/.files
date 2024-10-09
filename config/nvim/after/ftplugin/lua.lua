e5a9a7ee55332168760d7784837a49d02d1df3d5
ltp <leo@ltp.pt>
2024-10-09 19:41:57 -0300

    add ftplugin

---
+++ config/nvim/after/ftplugin/lua.lua
@@ -0,0 +1,6 @@
+local o = vim.opt
+
+o.expandtab = false
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
---
+++ config/nvim/after/ftplugin/nix.lua
@@ -0,0 +1,6 @@
+local o = vim.opt
+
+o.expandtab = true
+o.shiftwidth = 2
+o.tabstop = 2
+o.softtabstop = 2
