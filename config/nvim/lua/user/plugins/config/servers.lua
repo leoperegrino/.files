87f46366374bd38929cb8fc162caa31b5b2ffa13
ltp <leo@ltp.pt>
2024-12-06 08:33:22 -0300

    add current servers

--- config/nvim/lua/user/plugins/config/servers.lua
+++ config/nvim/lua/user/plugins/config/servers.lua
@@ -2,18 +2,19 @@ local M = {}
 
 M.nil_ls = {
 	-- https://github.com/oxalica/nil/blob/main/docs/configuration.md
+	cmd = { "nix", "run", "nixpkgs#nil" },
 	settings = {
 		["nil"] = {
 			formatting = {
-				command = { "nix", "run", "nixpkgs#alejandra" },
+				command = { "nix", "run", "nixpkgs#nixfmt-rfc-style" },
 			},
 			nix = {
 				-- maxMemoryMB = 2048,
 				flake = {
 					-- calls `nix flake archive` to put a flake and its output to store
-					autoArchive = true,
+					autoArchive = false,
 					-- auto eval flake inputs for improved completion
-					autoEvalInputs = true,
+					autoEvalInputs = false,
 					-- The input name of nixpkgs for NixOS options evaluation.
 					nixpkgsInputName = "nixpkgs",
 				},
@@ -23,6 +24,7 @@ M.nil_ls = {
 }
 
 M.lua_ls = {
+	cmd = { "nix", "run", "nixpkgs#lua-language-server" },
 	settings = {
 		Lua = {
 			diagnostics = { globals = { "vim", "require" }, },
@@ -34,25 +36,23 @@ M.lua_ls = {
 }
 
 M.texlab = {
-	settings = {
-		texlab = {
-			build = {
-				args = { "-v", "%f" },
-				executable = "arara",
-				forwardSearchAfter = true,
-				onSave = true
-			}
-		}
-	}
+	cmd = { "nix", "run", "nixpkgs#texlab" },
 }
 
--- pip install "python-lsp-server[all]
+-- pip install "python-lsp-server[all]"
 M.pylsp = {
-	-- cmd = { 'python',  '-m', 'pylsp' },
+	cmd = { 'python',  '-m', 'pylsp' },
+	-- cmd = { "nix", "run", "nixpkgs#python3Packages.python-lsp-server" },
+	-- cmd = {
+	-- 	"nix-shell",
+	-- 	"--impure",
+	-- 	"--expr", "let pkgs = import <nixpkgs> {}; in pkgs.mkShell { packages = [ (pkgs.python3.withPackages (pp: with pp; [ python-lsp-server ] ++ python-lsp-server.optional-dependencies.all)) ]; }",
+	-- 	"--run", "pylsp"
+	-- },
 	settings = {
 		pylsp = {
 			plugins = {
-				jedi_hover = { enabled = true },
+				jedi_hover = { enabled = false },
 
 				-- optional providers
 
@@ -66,7 +66,7 @@ M.pylsp = {
 				mccabe = { enabled = true },
 
 				-- linter for style checking
-				pycodestyle = { enabled = true },
+				pycodestyle = { enabled = false },
 
 				-- linter for docstring style checking (disabled by default)
 				pydocstyle = { enabled = false, convention = 'pep257', addIgnore = { 'D100' } },
@@ -87,9 +87,24 @@ M.pylsp = {
 	}
 }
 
-M.tsserver = {
+M.pyright = {
+	cmd = { "nix", "shell", "nixpkgs#pyright", "--command", "pyright-langserver", "--stdio" },
+}
+
+M.ruff = {
+	cmd = { "nix", "run", "nixpkgs#ruff-lsp" },
+	init_options = {
+		settings = {
+			-- Any extra CLI arguments for `ruff` go here.
+			args = {},
+		},
+	},
+}
+
+M.ts_ls = {
 	-- opts.filetypes = { "typescript", "typescriptreact", "typescript.tsx" }
-	init_options = { preferences = { disableSuggestions = true }},
+	cmd = { 'nix', 'run', 'nixpkgs#nodePackages.typescript-language-server', '--', '--stdio', },
+	init_options = { preferences = { disableSuggestions = false }},
 	settings = {
 		tsserver = {
 			compilerOptions = {
@@ -102,6 +117,24 @@ M.tsserver = {
 			}
 		}
 	}
+}
+
+M.metals = {
+	cmd = { 'metals' },
+	filetypes = {
+		'scala',
+		-- 'sbt',
+	},
+}
+
+M.terraformls = {
+	cmd = { "terraform-ls", "serve" },
+	filetypes = { "terraform", "terraform-vars" },
+}
+
+M.sqls = {
+	cmd = { 'nix', 'run', 'nixpkgs#sqls', '--',  '-config', '~/.config/sqls/config.yml' },
+	-- filetypes = {},
 }
 
 return M
