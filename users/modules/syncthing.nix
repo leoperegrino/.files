a08bab3e0de0d1c38953f9dda4c756ac1a3904b7
Leonardo Peregrino <55335068+leoperegrino@users.noreply.github.com>
2025-03-11 11:26:04 -0300

    home-manager syncthing does not support custom config dir

--- users/modules/syncthing.nix
+++ users/modules/syncthing.nix
@@ -1,7 +1,6 @@
 {lib, config, ... }:
 let
   cfg = config.modules.users.syncthing;
-  homeDirectory = config.home.homeDirectory;
 in {
 
   options.modules.users = {
@@ -14,8 +13,11 @@ in {
     services.syncthing = {
       enable = true;
       extraOptions = [
-        "-config=${homeDirectory}/syncthing/config/"
-        "-data=${homeDirectory}/syncthing/data/"
+        # -config must be ${stateHome}/syncthing for the syncthing-init.service to work
+        # https://github.com/nix-community/home-manager/pull/5616#issuecomment-2661610164
+        # https://github.com/nix-community/home-manager/blob/1c189f011447810af939a886ba7bee33532bb1f9/modules/services/syncthing.nix#L76
+        "-config=${config.xdg.stateHome}/syncthing/"
+        "-data=${config.xdg.cacheHome}/syncthing/"
       ];
       overrideFolders = true;
       overrideDevices = true;
