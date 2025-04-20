ff0202c6e0f98d9715e3b3f8b1533109c6e3c807
Leonardo Peregrino <55335068+leoperegrino@users.noreply.github.com>
2025-04-20 15:29:26 -0300

    split options into their scope

--- config/nvim/lua/user/core/options.lua
+++ config/nvim/lua/user/core/options.lua
@@ -4,9 +4,7 @@ local o = vim.opt
 local g = vim.g
 
 
-M.setup = function()
-	o.undofile = true
-	o.swapfile = true
+M.global = function()
 	o.backup = true
 	o.writebackup = true
 	o.backupdir:remove({ '.' })
@@ -14,62 +12,92 @@ M.setup = function()
 	o.autochdir = false
 	o.clipboard = 'unnamed'
 
-	o.fileencoding = 'utf-8'
-	o.fileformat = 'unix'
-
 	o.shortmess:append('AFI')
-	o.completeopt = { 'menu', 'menuone', 'noselect' }
 
-	-- o.startofline = true
 	o.updatetime = 300
 
-	o.signcolumn = 'yes:2'
-	o.cursorline = true
-	o.colorcolumn = '79'
-
-	o.wrap = false
 	o.laststatus = 3
 	o.showtabline = 2
 	o.showmode = false
 	o.showcmd = false
-	o.conceallevel = 3
 
-	o.foldmethod = 'syntax'
 	o.foldopen:append({ 'insert', 'jump' })
 	o.foldopen:remove({ 'block' })
-	o.foldminlines = 1
+
 	o.foldlevelstart = 99
-
-	o.shiftwidth = 4
-	o.tabstop = 4
-	o.softtabstop = 4
-
-	o.scrolloff = 7
-	o.sidescrolloff = 15
-
-	o.number = true
-	-- o.relativenumber = false
-
-	o.smartindent = true
-	o.cindent = true
-	o.cinkeys:remove({'0#'})
 
 	o.splitbelow = true
 	o.splitright = true
 
 	o.ignorecase = true
 	o.smartcase = true
-	o.infercase = true
 
 	o.wildmode:prepend({ 'longest' })
 	o.wildignorecase = true
 	o.wildignore:append({ '*.pyc', '*pycache*' })
+end
 
-	o.matchpairs:append({ '<:>' })
 
-	o.list = true
+M.global_local = function()
+	o.completeopt = { 'menu', 'menuone', 'noselect' }
+
+	o.scrolloff = 7
+	o.sidescrolloff = 15
+
 	o.listchars:append({ tab = '  ', trail = '~' })
 	o.fillchars:append({ fold = ' ', eob = ' ' })
+end
+
+
+M.buffer = function()
+	o.undofile = true
+	o.swapfile = true
+
+	o.fileencoding = 'utf-8'
+	o.fileformat = 'unix'
+
+	o.shiftwidth = 4
+	o.tabstop = 4
+	o.softtabstop = 4
+
+	o.smartindent = true
+	o.cindent = true
+	o.cinkeys:remove({'0#'})
+
+	o.infercase = true
+
+	o.matchpairs:append({ '<:>' })
+end
+
+
+M.window = function()
+	o.signcolumn = 'yes:2'
+
+	o.cursorline = true
+	o.colorcolumn = '79'
+
+	o.wrap = false
+
+	o.conceallevel = 3
+
+	o.foldenable = true
+	o.foldmethod = 'syntax'
+
+	o.foldminlines = 4
+
+	o.number = true
+	o.relativenumber = false
+
+	o.list = true
+end
+
+
+M.setup = function()
+	M.global()
+	M.global_local()
+	M.buffer()
+	M.window()
+
 
 	g.mapleader = ' '
 	g.xml_syntax_folding = 1
