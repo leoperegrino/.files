223d35830985d5eae2115cb8be3c577d35d6c45f
Leonardo Peregrino <55335068+leoperegrino@users.noreply.github.com>
2025-04-21 20:33:20 -0300

    start migration to lazy structure: rm keymaps

--- config/nvim/lua/user/plugins/init.lua
+++ config/nvim/lua/user/plugins/init.lua
@@ -1,13 +1,11 @@
 local M = {}
 
 local packager = require('user.plugins.packager')
-local keymaps = require('user.plugins.keymaps')
 local lsp = require('user.plugins.lsp')
 
 
 M.setup = function()
 	packager.setup()
-	keymaps.setup()
 	lsp.setup()
 end
 
