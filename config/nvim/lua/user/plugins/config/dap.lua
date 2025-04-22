ebc6a554314849735579d073e736d08b7ca6b809
Leonardo Peregrino <55335068+leoperegrino@users.noreply.github.com>
2025-04-22 11:20:33 -0300

    refactored dap module to use lazy spec

--- config/nvim/lua/user/plugins/config/dap.lua
+++ config/nvim/lua/user/plugins/config/dap.lua
@@ -1,76 +1,39 @@
-local function keymap(mode, lhs, rhs, desc)
-	vim.keymap.set(mode, lhs, rhs, {
-		desc = 'dap: ' .. desc,
-		noremap = true,
-		silent = true,
-	})
-end
-
-
--- TODO: configure this according to lazy spec (keys, opts, buffers, config, etc)
 return {
 
 	{ 'mfussenegger/nvim-dap',
-		config = function(_, _)
-			local dap = require("dap")
-			local telescope = require('telescope.builtin')
-
-			vim.fn.sign_define("DapBreakpoint", { text = "🌕"})
-			vim.fn.sign_define("DapStopped", { text = "🌑"})
-
-			keymap('n', '<leader>dk',
-				function() telescope.keymaps({ default_text = 'dap: ' }) end,
-				"keymaps"
-			)
-			keymap('n', '<leader>dK',
-				function() telescope.commands({ default_text = 'Dap' }) end,
-				"commands"
-			)
-
-			keymap('n', '<leader>dc', dap.continue , "continue")
-
-			-- keymap('n', '<leader>dC', dap.run, "run")
-
-			keymap('n', '<F5>', dap.step_into        , "step into")
-			keymap('n', '<F6>', dap.step_out         , "step out")
-			keymap('n', '<F7>', dap.step_over        , "step over")
-			keymap('n', '<F8>', dap.toggle_breakpoint, "toggle breakpoint")
-			keymap('n', '<F9>', dap.continue         , "continue")
-
-			keymap('n', '<leader>do', dap.step_over, "step over")
-			keymap('n', '<leader>di', dap.step_into, "step into")
-			keymap('n', '<leader>dO', dap.step_out , "step out")
-			keymap('n', '<leader>dU', dap.up       , "go up in the stack")
-			keymap('n', '<leader>dD', dap.down     , "go down in the stack")
-
-			keymap('n', '<leader>dR', dap.restart  , "restart session")
-			keymap('n', '<leader>dt', dap.terminate, "terminate session")
-			keymap('n', '<leader>dP', dap.pause    , "pause a thread")
-
-			keymap('n', '<leader>dr', dap.repl.toggle, "toggle repl")
-
-			keymap('n', '<leader>dbb', dap.toggle_breakpoint, "toggle breakpoint")
-			-- keymap('n', '<leader>dbl', dap.list_breakpoints , "list breakpoints")
-			keymap('n', '<leader>dbc', dap.clear_breakpoints , "clear breakpoints")
-
-			keymap('n', '<leader>dbB',
-				function() dap.set_breakpoint(nil, nil, vim.fn.input('message: ')) end,
-				"add breakpoint with comment"
-			)
-		end
+		init = function()
+			vim.fn.sign_define("DapBreakpoint", { text = "🌕" })
+			vim.fn.sign_define("DapStopped", { text = "🌑" })
+		end,
+		keys = {
+			{ '<leader>dc' , function() require("dap").continue() end, desc = 'dap: continue' },
+			{ '<leader>do' , function() require("dap").step_over() end, desc = 'dap: step over' },
+			{ '<leader>di' , function() require("dap").step_into() end, desc = 'dap: step into' },
+			{ '<leader>dO' , function() require("dap").step_out() end, desc = 'dap: step out' },
+			{ '<leader>dU' , function() require("dap").up() end, desc = 'dap: go up in the stack' },
+			{ '<leader>dD' , function() require("dap").down() end, desc = 'dap: go down in the stack' },
+			{ '<leader>dR' , function() require("dap").restart() end, desc = 'dap: restart session' },
+			{ '<leader>dt' , function() require("dap").terminate() end, desc = 'dap: terminate session' },
+			{ '<leader>dP' , function() require("dap").pause() end, desc = 'dap: pause a thread' },
+			{ '<leader>dr' , function() require("dap").repl.toggle() end, desc = 'dap: toggle repl' },
+			{ '<leader>dbb', function() require("dap").toggle_breakpoint() end, desc = 'dap: toggle breakpoint' },
+			{ '<leader>dbc', function() require("dap").clear_breakpoints() end, desc = 'dap: clear breakpoints' },
+		}
 	},
 
 	{ 'mfussenegger/nvim-dap-python',
 		dependencies = { { 'mfussenegger/nvim-dap' }, },
-		config = function(_, _)
+		config = function()
 			local dap_python = require("dap-python")
-
-			keymap('n', '<leader>dpm', dap_python.test_method, "python: test method")
-			keymap('n', '<leader>dpc', dap_python.test_class, "python: test class")
 
 			dap_python.test_runner = "pytest"
 			dap_python.setup("python")
 		end,
+		keys = {
+			{ '<leader>dpm', function() require("dap-python").test_method() end, desc = "dap-python: test method"},
+			{ '<leader>dpc', function() require("dap-python").test_class() end, desc = "dap-python: test class"},
+
+		},
 	},
 
 	{ 'theHamsta/nvim-dap-virtual-text',
@@ -80,51 +43,29 @@ return {
 
 	{ "rcarriga/nvim-dap-ui",
 		dependencies = { { "mfussenegger/nvim-dap" }, { "nvim-neotest/nvim-nio" }, },
-		config = function()
+		opts = {
+			layouts = {
+				{ elements = { { id = "scopes", size = 1 }, }, position = "left", size = 30 },
+				-- { elements = { { id = "repl", size = 1 }, }, position = "bottom", size = 10 },
+				{ elements = { { id = "console", size = 1 } }, position = "bottom", size = 2 },
+			},
+		},
+		keys = {
+			{ '<leader>dd', function() require("dapui").toggle() end, desc = 'dapui: toggle ui' },
+			{ '<leader>de', function() require("dapui").eval() end, desc = 'dapui: eval expression', mode = {'n', 'v'} },
+			{ '<leader>df', function() require("dapui").float_element('scopes') end, desc = 'dapui: open floating scopes' },
+		},
+		config = function(_, opts)
 			local dap = require("dap")
+			local before = dap.listeners.before
 			local dapui = require("dapui")
 
-			keymap("n"       , "<leader>dd", dapui.toggle, "dapui: toggle ui")
-			keymap({"n", "v"}, "<leader>de", dapui.eval  , "dapui: eval expression")
+			dapui.setup(opts)
 
-			keymap("n", "<leader>df",
-				function() dapui.float_element("scopes") end,
-				"dapui: open floating scopes"
-			)
-
-			dapui.setup({
-				-- breakpoints, stacks, watches, scopes, repl, console
-				layouts = {
-					{
-						elements = { { id = "scopes", size = 1 }, },
-						position = "left",
-						size = 30
-					},
-					-- {
-					-- 	elements = { { id = "repl", size = 1 }, },
-					-- 	position = "bottom",
-					-- 	size = 10
-					-- },
-					{
-						elements = { { id = "console", size = 1 } },
-						position = "bottom",
-						size = 2
-					},
-				}
-			})
-
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				-- dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				-- dapui.close()
-			end
+			before.attach.dapui_config = function() dapui.open() end
+			before.launch.dapui_config = function() dapui.open() end
+			-- before.event_terminated.dapui_config = function() dapui.close() end
+			-- before.event_exited.dapui_config = function() dapui.close() end
 		end,
 	},
 }
