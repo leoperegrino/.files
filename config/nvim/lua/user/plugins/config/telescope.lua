61b91edf563bc44aebcd2b487106e36071828c13
Leonardo Peregrino <55335068+leoperegrino@users.noreply.github.com>
2025-05-18 16:51:59 -0300

    defaults are enough

--- config/nvim/lua/user/plugins/config/telescope.lua
+++ config/nvim/lua/user/plugins/config/telescope.lua
@@ -74,25 +74,6 @@ return {
 				},
 				initial_mode = 'insert',
 				scroll_strategy = "limit",
-				layout_config = {
-					width = 0.95,
-					height = 0.85,
-					prompt_position = "top",
-					horizontal = {
-						preview_width = function(_, cols, _)
-							if cols > 200 then
-								return math.floor(cols * 0.4)
-							else
-								return math.floor(cols * 0.6)
-							end
-						end,
-					},
-					vertical = {
-						width = 0.9,
-						height = 0.95,
-						preview_height = 0.5,
-					},
-				},
 			}
 		},
 		keys = function()
