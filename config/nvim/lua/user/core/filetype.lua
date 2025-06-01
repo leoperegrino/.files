33b5e9f3f4d74d642cc50d0a01835de0513f4132
Leonardo Peregrino <55335068+leoperegrino@users.noreply.github.com>
2025-06-01 16:17:37 -0300

    add terraform extension filetype

--- config/nvim/lua/user/core/filetype.lua
+++ config/nvim/lua/user/core/filetype.lua
@@ -2,6 +2,9 @@ local M = {}
 
 M.setup = function()
 	vim.filetype.add({
+		extension = {
+			tf = 'terraform',
+		},
 		filename = {
 			['docker-compose.yaml'] = 'yaml.docker-compose',
 			['docker-compose.yml'] = 'yaml.docker-compose',
