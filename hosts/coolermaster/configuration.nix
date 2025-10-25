d812aa8d868d9836dd36ab1a4451156ddaa54ac2
Leonardo Peregrino <55335068+leoperegrino@users.noreply.github.com>
2025-10-25 12:23:19 -0300

    configuration cleanup

--- hosts/coolermaster/configuration.nix
+++ hosts/coolermaster/configuration.nix
@@ -75,11 +75,6 @@ in {
         10001
         3478
       ];
-      checkReversePath = true;
-      logRefusedConnections = false;
-      logRefusedPackets = false; # this generates a lot of log
-      logRefusedUnicastsOnly = false;
-      logReversePathDrops = false;
     };
   };
 
@@ -101,7 +96,7 @@ in {
   users = {
     users."cool" = {
       openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCzXCQtXnv7icwj2pYbYiifx0EtuOzX8L5PPVFKsFyIg40m3et2eJmNXdzgrhIiPt4ZHQ7VmC22ZxHzOuYNHT/bPR5DrL/DDuAblJgGdc/alkx3MaSuYDcIBi133mQpIHPNUJU2B97tWqXQsvpvIalsWR5UpGLyGfUU03zIemwBbZ4OtCAUn5FMpoSi5v4wsWxJ8vgiiT4/lj7adK1YivKTN+2B1eOxjiUukD0sEifxhlWXZ6p9mQd3d3JY0tLvONhwmxAWQ2L9LbIFOkNnYt5eDeOho5k+Bid8TT0cfc1MBV+qumfYe6KnFSH0TJc7kjnSS2N0s4Ue3h3UeY+b16g2bsipoRH0YP0NGEqr4+oNfIF0sJ0RVOo6qQs6kxx04aweEfatNgBnYpM8ItHPVX7yw787wPatWCJoKqr3GvN9rjSRoS7YeR/elr42Cs6c/zzqcnjLjs9etdLGyH38fSI/pZKLwqoVDH/gal/GShQz7jzJGygVEL+rs0EsFCVUpwDlIIuE/HdE/Nyon6Ufl3q9IvSf0fbmwgtL6q7dYGA8bUXSVxl2uDj35b6Yh2N9huRPOX8WciOvpDXvvc6GqkcuZmoSPETDqlPgvpfFQSMEfIBlUtFePVX207g1yP067RIbJnYIy+CITemlvjWwU1kzsucFxkx3QVdcyJmpL42MAw== ltp@nixos"
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCzXCQtXnv7icwj2pYbYiifx0EtuOzX8L5PPVFKsFyIg40m3et2eJmNXdzgrhIiPt4ZHQ7VmC22ZxHzOuYNHT/bPR5DrL/DDuAblJgGdc/alkx3MaSuYDcIBi133mQpIHPNUJU2B97tWqXQsvpvIalsWR5UpGLyGfUU03zIemwBbZ4OtCAUn5FMpoSi5v4wsWxJ8vgiiT4/lj7adK1YivKTN+2B1eOxjiUukD0sEifxhlWXZ6p9mQd3d3JY0tLvONhwmxAWQ2L9LbIFOkNnYt5eDeOho5k+Bid8TT0cfc1MBV+qumfYe6KnFSH0TJc7kjnSS2N0s4Ue3h3UeY+b16g2bsipoRH0YP0NGEqr4+oNfIF0sJ0RVOo6qQs6kxx04aweEfatNgBnYpM8ItHPVX7yw787wPatWCJoKqr3GvN9rjSRoS7YeR/elr42Cs6c/zzqcnjLjs9etdLGyH38fSI/pZKLwqoVDH/gal/GShQz7jzJGygVEL+rs0EsFCVUpwDlIIuE/HdE/Nyon6Ufl3q9IvSf0fbmwgtL6q7dYGA8bUXSVxl2uDj35b6Yh2N9huRPOX8WciOvpDXvvc6GqkcuZmoSPETDqlPgvpfFQSMEfIBlUtFePVX207g1yP067RIbJnYIy+CITemlvjWwU1kzsucFxkx3QVdcyJmpL42MAw== ltp@thinkpad"
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCp33NKY9pJxYu1wgV/vemJwAHiWsSfmf3n39y1PU/yQmOHbTmBmogJ0xIp0YbVtdHI3G7qg5XkIUffTOAOVHaifrSeknFK9xzhFFI0idGKcbS1djdXiYhCQjDI3OxXx8G47LP1UpVmF1QWcQRWUvVESUtMAnQ3IwlSNhovAOa/YFRcKCi1C24gPeEzJQCnb4daw+YcrMfv3vieBrhlIZaOOoYDqx8zvUNh+hp5Ys4YvPy+wVEYAl3bzA+NJO60mFv1SzoIUZZkBLTUjpR+VCkI9jljEGe7VZchOW/8RXDaGmE9DRBaOqms6VTms2ePmlqDIGCgeYw1nWxsc9XV+tH3TVrfaQ//ACYE3PzRo0uwFW2dI5Ph6scffKoQ2BbAhZbs/KYvlQt+ru6P8P1bKiUMxUAzX5jzgo9ammue8BtTsRJ2Vb1v/I2d155njfKv/oNwS++47FJ9gKpvZuyRtrDzSCYPxWuclLliR3b4+5nyMKkVMMfpjn5mCV1bVGHKvY0= u0_a129@localhost"
       ];
       isNormalUser = true;
