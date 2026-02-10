e263379e67f1f51e5c5aa6c0af391e78ef841a56
Leonardo Peregrino <55335068+leoperegrino@users.noreply.github.com>
2026-02-09 23:28:37 -0300

    add xdg variables to session

--- users/modules/xdg.nix
+++ users/modules/xdg.nix
@@ -50,5 +50,12 @@ in
       };
     };
 
+    home.sessionVariables = {
+      XDG_CACHE_HOME = "${config.xdg.cacheHome}";
+      XDG_CONFIG_HOME = "${config.xdg.configHome}";
+      XDG_DATA_HOME = "${config.xdg.dataHome}";
+      XDG_STATE_HOME = "${config.xdg.stateHome}";
+    };
+
   };
 }
